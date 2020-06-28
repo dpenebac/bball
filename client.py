37671f457656b82dd4d0652432be6748d3fdd9a4
Ubuntu <me@gmail.com>
2020-06-28 12:17:11 -1000

    added tkinter data

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import socket
+import tkinter as tk
+from tkinter import *
 
 PORT = 5050
 SERVER = "192.168.1.214"
@@ -8,10 +10,24 @@ FORMAT = 'utf-8'
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect(ADDR)
 
-def sendInfo(info):
-    b = info.encode(FORMAT)
-    client.send(b)
+class app:
+	def __init__(self,master):
+		self.master = master
+		self.master.attributes('-fullscreen',True)
+		self.makeButton = Button(master, text = "Make", height = 30, width = 30, command = lambda: sendData(1))
+		self.missButton = Button(master, text = "Miss", height = 30, width = 30, command = lambda: sendData(0))
+		self.quitButton = Button(master, text = "Quit", command = lambda: sendData(-1))
+		self.makeButton.pack(side = LEFT)
+		self.missButton.pack(side = RIGHT)
+		self.quitButton.pack()
 
-while 1:
-    info = input()
-    sendInfo(info)
+def sendData(info):
+    if info != -1:
+        b = info.encode(FORMAT)
+        client.send(b)
+    else:
+        sys.exit()
+
+root = Tk()
+gui = app(root)
+root.mainloop()
